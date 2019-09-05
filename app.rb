8855cabde24df8bc4eebc3ae73a185db47f17293
Морозов Иван Александрович <ivan.morozov@tele2.ru>
2019-09-05 11:05:50 +0600

    Add /post to visit and users list

--- app.rb
+++ app.rb
@@ -14,3 +14,20 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+
+	@username  = params[:username]
+	@phone     = params[:phone]
+	@datetime  = params[:datetime]
+
+	@title = 'Thank you!'
+	@message = "Dear #{@username}, we'll be waiting for you at #{@datetime}"
+
+	f = File.open './public/users.txt', 'a'
+	f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime}\n"
+	f.close
+
+	erb :message
+
+end
